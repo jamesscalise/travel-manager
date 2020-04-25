3380c429bd5bf1b4f73d5536d4a8c88dde01d047
James <jamescscalise@gmail.com>
2020-04-25 18:49:04 -0500

    create readme

---
+++ README.md
@@ -0,0 +1,6 @@
+This is an application where the user can add destinations to visit, and sights to see at that destination
+
+By clicking on destinations, the sights associated to the destination expand, in addition to a form for 
+create new sights associated with the destination
+
+The user can delete any sight associated with a destination
--- backend/app/controllers/sites_controller.rb
+++ backend/app/controllers/sites_controller.rb
@@ -11,5 +11,16 @@ class SitesController < ApplicationController
 
       def create
         site = Site.create(name: params[:name], destination_id: params[:destination_id])
+        if site.save
+          render json: SiteSerializer.new(site)
+        else
+          render json: {message: "Error! Sight couldn't be create in controller!"}
+        end
+      end
+
+      def destroy
+        site = Site.find_by(id: params[:id])
+        site.destroy
+        
       end
 end
